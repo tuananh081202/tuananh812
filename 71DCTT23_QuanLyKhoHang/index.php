63bf0603c236c4b95cd6657094622502fdbaadff
tuananh081202 <bachutoanquoc0812@gmail.com>
2023-12-07 17:35:19 +0700

    a

--- 71DCTT23_QuanLyKhoHang/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-// require_once bị bỏ qua nếu tệp bắt buộc 
-// đã được thêm bởi bất kỳ câu lệnh nào trong bốn câu lệnh include
-require_once "./mvc/bridge.php";
-// Gọi new .. hàm __construct() sẽ chạy
-$myApp = new App();
-?> 
--- 71DCTT23_QuanLyKhoHang/mvc/views/Login.php
+++
@@ -1,1 +0,0 @@
-<?php require_once "./mvc/views/pages/".$data["page"].".php"; ?>
